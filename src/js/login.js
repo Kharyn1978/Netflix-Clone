b1d5e303f0cfe3911d348d5a0158be0b83c98c6c
K.Kharyn Ilunga <95341639+Kharyn1978@users.noreply.github.com>
2022-07-24 12:26:45 +0200

    session and local storage

--- src/js/login.js
+++ src/js/login.js
@@ -15,16 +15,19 @@ const users = [
         password: "pass3"
     }
 ];
-
+sessionStorage.clear();
 
 if (localStorage.getItem("allUsers") === null)
     localStorage.setItem("allUsers",JSON.stringify(users));
 
-function login() {
+function login(e) {
+    //e.preventDefault();
     let storedUsers = JSON.parse(localStorage.getItem("allUsers"));
     //let formData = document.getElementById("signInForm"); //for the form
     let thisEmail = document.getElementById("userEmail").value;
     let thisPassword = document.getElementById("userPassword").value;
+    let detailsCorrect = false;
+    let matchedUser = {};
 
     console.log(thisEmail)
     for (let i = 0; i < storedUsers.length; i++) {
@@ -33,91 +36,107 @@ function login() {
             storedUsers[i].password !== thisPassword
         ) {
             //show error -- enter corrwectemail passowoerrd
-            alert("please enter correct details");
-            return;
+            detailsCorrect = false;
         }
 
         if (
             storedUsers[i].email === thisEmail || 
             storedUsers[i].password === thisPassword
         ) {
-            //we login with user
-
-            let thisUser = localStorage.getItem(storedUsers[i].id);
-            alert(JSON.parse(thisUser));
+            detailsCorrect = true;
+            matchedUser = storedUsers[i];
+            break;
         }
+        console.log(detailsCorrect)
+    }
+
+    if (!detailsCorrect) {
+        alert("Please enter the correct details");
+    }
+
+    if (detailsCorrect) {
+        let thisUser = JSON.parse(localStorage.getItem(matchedUser.id));
+
+        let thisUserInfo = {};
+        console.log(thisUser);
+
+        allUsersMovieData.forEach(item => {
+            if (item.id === matchedUser.id) {
+                thisUserInfo = item;
+            }
+        })
+        sessionStorage.setItem(matchedUser.id, JSON.stringify(thisUserInfo));
+        window.location.replace("index.html");
+        //sessionStorage.setItem("user1",user1 movies)
     }
 }
 
-let user1 = {
-    id: "user1",
-    movies: [
-        {
-            Title: "Prince of Persia: The Sands of Time",
-            watched: true,
-            Available: true,
-        },
-        {
-            Title: "Spider-Man: Homecoming",
-            watched: true,
-            Available: true,
-        },
-        {
-            Title: "Indiana Jones and the Kingdom of the Crystal Skull",
-            watched: true,
-            Available: true,
-        },
-    ]
+function logout() {
+    sessionStorage.clear();
+    window.location.replace("form.html");
 }
 
+let allUsersMovieData = [
+    {
+        id: "user1",
+        movies: [
+            {
+                Title: "Prince of Persia: The Sands of Time",
+                watched: true,
+                Available: true,
+            },
+            {
+                Title: "Spider-Man: Homecoming",
+                watched: true,
+                Available: true,
+            },
+            {
+                Title: "Indiana Jones and the Kingdom of the Crystal Skull",
+                watched: true,
+                Available: true,
+            },
+        ]
+    },
+    {
+        id: "user2",
+        movies: [
+            {
+                Title: "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe",
+                watched: true,
+                Available: true,
+            },
+            {
+                Title: "King Kong Lives",
+                watched: true,
+                Available: true,
+            },
+            {
+                Title: "Jumanji: The Next Level",
+                watched: true,
+                Available: true,
+            },
 
+        ]
+    },
+    {
+        id: "user3",
+        movies: [
+            {
+                Title: "The Fast and the Furious: Tokyo Drift",
+                watched: true,
+                Available: true,
+            },
+            {
+                Title: "Skyfall",
+                watched: true,
+                Available: true,
+            },
+            {
+                Title: "Spectre",
+                watched: true,
+                Available: true,
+            },
 
-localStorage.setItem("user1",JSON.parse(user1));
-
-let User2 = {
-    id: "user2",
-    movies: [
-        {
-            Title: "The Chronicles of Narnia: The Lion, the Witch and the Wardrobe",
-            watched: true,
-            Available: true,
-        },
-        {
-            Title: "King Kong Lives",
-            watched: true,
-            Available: true,
-        },
-        {
-            Title: "Jumanji: The Next Level",
-            watched: true,
-            Available: true,
-        },
-
-    ]
-}
-
-localStorage.setItem("user2",JSON.parse(user2));
-
-let user3 = {
-    id: "user3",
-    movies: [
-        {
-            Title: "The Fast and the Furious: Tokyo Drift",
-            watched: true,
-            Available: true,
-        },
-        {
-            Title: "Skyfall",
-            watched: true,
-            Available: true,
-        },
-        {
-            Title: "Spectre",
-            watched: true,
-            Available: true,
-        },
-
-    ]
-}
-
-localStorage.setItem("user3",JSON.parse(user3));
+        ]
+    }
+];
